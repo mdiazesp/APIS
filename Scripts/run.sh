83aa3bd4f19cd33adc0a7e5d505285877d0bcc4d
mdiazesp <narutomde@gmail.com>
2024-02-20 23:09:30 +0000

    APIS

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -6,7 +6,7 @@ docker stop test_api01 || true
 
 #docker run -itd --name test_api01  -p 3001:3000 --rm app:1.0
 
-docker run -itd --name test_api01  -p 3001:3000 --rm fjcuamatzi/api_cdk_ene2024:$1
+docker run -itd --name test_api01 -p 3001:3000 --rm mdiaze/api_cdk_ene2024:$1
 
 
 
